a57edf5a5d28d252e3d96b08d3916b53a74cdff2
frank <frank@dnvgl>
2017-05-27 14:12:33 +0800

    Initial Commit

---
+++ MyDNVGLNotificationDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyDNVGLNotificationDemo
+//
+//  Created by Dnvgl on 27/05/2017.
+//  Copyright © 2017 Dnvgl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyDNVGLNotificationDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  MyDNVGLNotificationDemo
+//
+//  Created by Dnvgl on 27/05/2017.
+//  Copyright © 2017 Dnvgl. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
